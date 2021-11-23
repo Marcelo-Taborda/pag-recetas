c16b62c4d80bc33ef53be888d4cbb6c2b3fc28cd
Marcelo Taborda <86986422+Marcelo-Taborda@users.noreply.github.com>
2021-11-22 22:38:03 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,36 +1,32 @@
-alert("¿Esta dispuesto a ver el mejor sitio del mundo mundial?");
-document.onload = () => {
-    const cards = document.querySelectorAll(".card-body");
-    const boton = document.querySelector(".modoBW").children[0];
-    const main = document.querySelector(".todo").children;
-    let modoOscuro = false;
-    boton.addEventListener("click", (e) => {
-        if (!modoOscuro) {
-            e.target.textContent = "Modo claro";
-            modoOscuro = true;
-        } else {
-            e.target.textContent = "Modo oscuro";
-            modoOscuro = false;
-        }
+const cards = document.querySelectorAll(".card-body");
+const boton = document.querySelector(".modoBW").children[0];
+const main = document.querySelector(".todo").children;
 
-        cards.forEach(card => {
-            card.classList.toggle("modoOscuro");
-        });
+let modoOscuro = false;
+boton.addEventListener("click", (e) => {
+    if (!modoOscuro) {
+        e.target.textContent = "Modo claro";
+        modoOscuro = true;
+    } else {
+        e.target.textContent = "Modo oscuro";
+        modoOscuro = false;
+    }
 
-        main[0].classList.toggle("modoOscuro");
-        main[1].classList.toggle("modoOscuro");
-        main[1].querySelectorAll("a").forEach(element => {
-            element.classList.toggle("modoOscuro")
-        });
-    })
-}
-
-
+    cards.forEach(card => {
+        card.classList.toggle("modoOscuro");
+    });
+    
+    main[0].classList.toggle("modoOscuro");
+    main[1].classList.toggle("modoOscuro");
+    main[1].querySelectorAll("a").forEach(element => {
+        element.classList.toggle("modoOscuro")
+    });
+})
 
 datosCreador = () => {
     alert(
         "Creador: Marcelo Taborda\n" +
         "Edad: 20 años\n" +
         "Pais: Argentina\n"
-    )
+    );
 }
