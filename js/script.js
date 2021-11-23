1841848d50a895325fe76b7051caa26d66c00f81
Marcelo Taborda <86986422+Marcelo-Taborda@users.noreply.github.com>
2021-11-22 22:35:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
 alert("¿Esta dispuesto a ver el mejor sitio del mundo mundial?");
-const cards = document.querySelectorAll(".card-body");
-const boton = document.querySelector(".modoBW").children[0];
-const main = document.querySelector(".todo").children;
-let modoOscuro = false;
-boton.addEventListener("click", (e) => {
-    if (!modoOscuro) {
-        e.target.textContent = "Modo claro";
-        modoOscuro = true;
-    } else {
-        e.target.textContent = "Modo oscuro";
-        modoOscuro = false;
-    }
+document.onload = () => {
+    const cards = document.querySelectorAll(".card-body");
+    const boton = document.querySelector(".modoBW").children[0];
+    const main = document.querySelector(".todo").children;
+    let modoOscuro = false;
+    boton.addEventListener("click", (e) => {
+        if (!modoOscuro) {
+            e.target.textContent = "Modo claro";
+            modoOscuro = true;
+        } else {
+            e.target.textContent = "Modo oscuro";
+            modoOscuro = false;
+        }
 
-    cards.forEach(card => {
-        card.classList.toggle("modoOscuro");
-    });
-    
-    main[0].classList.toggle("modoOscuro");
-    main[1].classList.toggle("modoOscuro");
-    main[1].querySelectorAll("a").forEach(element => {
-        element.classList.toggle("modoOscuro")
-    });
-})
+        cards.forEach(card => {
+            card.classList.toggle("modoOscuro");
+        });
+
+        main[0].classList.toggle("modoOscuro");
+        main[1].classList.toggle("modoOscuro");
+        main[1].querySelectorAll("a").forEach(element => {
+            element.classList.toggle("modoOscuro")
+        });
+    })
+}
+
 
 
 datosCreador = () => {
